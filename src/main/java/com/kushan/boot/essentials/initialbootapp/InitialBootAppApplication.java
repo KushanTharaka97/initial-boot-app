74c2478d4d8587faea1e8038c65b91f68a54a664
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-25 05:15:49 +0530

    Initial Comment springbootApp changed the server.port to 8000 instead of 8080

---
+++ src/main/java/com/kushan/boot/essentials/initialbootapp/InitialBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.kushan.boot.essentials.initialbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushan/boot/essentials/initialbootapp/InitialBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushan.boot.essentials.initialbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
