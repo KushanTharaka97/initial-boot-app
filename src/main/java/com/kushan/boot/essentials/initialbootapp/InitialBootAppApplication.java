fccba5b80b0f2a910a6811ef22b3119883d5a75f
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-25 13:35:02 +0530

    SSL created

--- src/main/java/com/kushan/boot/essentials/initialbootapp/InitialBootAppApplication.java
+++ src/main/java/com/kushan/boot/essentials/initialbootapp/InitialBootAppApplication.java
@@ -2,12 +2,24 @@ package com.kushan.boot.essentials.initialbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class InitialBootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InitialBootAppApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(InitialBootAppApplication.class, args);}
+
+		@RestController
+		@RequestMapping("/api")
+		public class ApiController{
+		@GetMapping("/greeting")
+			public String getGreetings(){
+				return "Hello world from API";
+			}
+		}
+
+
 
 }
